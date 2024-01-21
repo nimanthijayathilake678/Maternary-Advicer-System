411dfc5baf0f26204896ceb20d2c1a41558b7872
nimanthijayathilake678 <jayathilake.nimanthi99@gmail.com>
2024-01-21 12:38:43 +0530

    Add all basic files

---
+++ src/main/java/group13/MaternaryAdvicerSystem/MaternaryAdvicerSystemApplication.java
@@ -0,0 +1,13 @@
+package group13.MaternaryAdvicerSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaternaryAdvicerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaternaryAdvicerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group13/MaternaryAdvicerSystem/MaternaryAdvicerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package group13.MaternaryAdvicerSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaternaryAdvicerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
