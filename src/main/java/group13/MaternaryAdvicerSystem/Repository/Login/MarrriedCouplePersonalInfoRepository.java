ec104a929fd8b02b1c319f38934b352b20ee70f9
Shanilka Gurusinghe <128635738+SMadu99@users.noreply.github.com>
2024-02-19 22:44:45 +0530

    add MarriedCouplePersonalinfo table

---
+++ src/main/java/group13/MaternaryAdvicerSystem/Repository/Login/MarrriedCouplePersonalInfoRepository.java
@@ -0,0 +1,8 @@
+package group13.MaternaryAdvicerSystem.Repository.Login;
+
+import group13.MaternaryAdvicerSystem.Model.Domain.MarrriedCouplePersonalInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarrriedCouplePersonalInfoRepository extends JpaRepository<MarrriedCouplePersonalInfo,String> {
+
+}
