ac94d2872143069a4b41315fe37d59eea9f4305d
Binishi-0822 <binishipiyumika3@gmail.com>
2024-04-10 21:50:55 +0530

    C_Housing_And_Workspace_Repository

---
+++ src/main/java/group13/MaternaryAdvicerSystem/Repository/Login/C_Housing_And_Workspace_Repository.java
@@ -0,0 +1,7 @@
+package group13.MaternaryAdvicerSystem.Repository.Login;
+
+import group13.MaternaryAdvicerSystem.Model.Domain.C_Housing_And_Workspace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface C_Housing_And_Workspace_Repository extends JpaRepository<C_Housing_And_Workspace,Long>{
+}
