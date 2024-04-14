3a27aada12fd3d6e50561a970effb1223d5245a0
Binishi-0822 <binishipiyumika3@gmail.com>
2024-04-14 15:06:18 +0530

    add C_Midwife_Remark_Repository

---
+++ src/main/java/group13/MaternaryAdvicerSystem/Repository/Login/C_Midwife_Remark_Repository.java
@@ -0,0 +1,7 @@
+package group13.MaternaryAdvicerSystem.Repository.Login;
+
+import group13.MaternaryAdvicerSystem.Model.Domain.C_Midwife_Remark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface C_Midwife_Remark_Repository extends JpaRepository<C_Midwife_Remark,Long> {
+}
