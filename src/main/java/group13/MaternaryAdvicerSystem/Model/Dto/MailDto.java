4fb20cf17712ea20cd01a2ff5432fc61bb59d107
nimanthijayathilake678 <jayathilake.nimanthi99@gmail.com>
2024-04-22 10:00:59 +0530

    Add Forgot password Files

---
+++ src/main/java/group13/MaternaryAdvicerSystem/Model/Dto/MailDto.java
@@ -0,0 +1,15 @@
+package group13.MaternaryAdvicerSystem.Model.Dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MailDto {
+    private String username;
+
+    private String password;
+}
